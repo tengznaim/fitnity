841685ece8ad1c8d20b1ff74a9373439325eafe4
Tengku Naim <naimtmas@gmail.com>
2022-08-26 18:08:15 +0800

    updated navigation components

---
+++ app/src/components/SideNav/SideNav.js
@@ -0,0 +1,34 @@
+import { FiPenTool } from "react-icons/fi";
+import { AiFillHome, AiFillTrophy } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
+import styles from "./sidenav.module.css";
+
+function SideNav() {
+  return (
+    <div className={styles.sidenav}>
+      <NavLink
+        to="/"
+        className={({ isActive }) =>
+          isActive ? `${styles.linkItem} ${styles.activeLink}` : styles.linkItem
+        }>
+        <AiFillHome className={styles.navIcon} />
+      </NavLink>
+      <NavLink
+        to="/create-event"
+        className={({ isActive }) =>
+          isActive ? `${styles.linkItem} ${styles.activeLink}` : styles.linkItem
+        }>
+        <FiPenTool className={styles.navIcon} />
+      </NavLink>
+      <NavLink
+        to="/sports-of-fame"
+        className={({ isActive }) =>
+          isActive ? `${styles.linkItem} ${styles.activeLink}` : styles.linkItem
+        }>
+        <AiFillTrophy className={styles.navIcon} />
+      </NavLink>
+    </div>
+  );
+}
+
+export default SideNav;
